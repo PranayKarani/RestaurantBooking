378db18bf3ae3aeea49fbdbf841c31380b4f11da
pranaykarani <pranay@reach52.com>
2022-04-28 21:19:39 +0200

    Updated project setup

---
+++ app/src/main/java/com/se/App.java
@@ -0,0 +1,19 @@
+
+package com.se;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello!");
+	}
+}
