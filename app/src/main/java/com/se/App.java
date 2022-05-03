30709721e3b393c05920b9d2d392d99f42665d89
pranaykarani <pranay@reach52.com>
2022-05-03 14:14:11 +0200

    Added Question class

--- app/src/main/java/com/se/App.java
+++ app/src/main/java/com/se/App.java
@@ -1,19 +1,41 @@
 
 package com.se;
 
-import java.io.IOException;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
-
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
 
 public class App {
 
-	public static void main(String[] args) {
-		System.out.println("Hello!");
-	}
+    public static void main(String[] args) {
+
+
+    }
+
+    // Only for testing purpose. Delete this later on...
+    private static void testQuestions() {
+        ArrayList<Question> questions = new ArrayList<>();
+        questions.add(new Question("What is your name", Question.Format.TXT));
+        questions.add(new Question("What is your age", Question.Format.NUM));
+        questions.add(new Question("Do you like pizza?", Question.Format.YN));
+
+        Scanner sc = new Scanner(System.in);
+
+        for (Question q : questions) {
+            System.out.println(q.text);
+
+            while (true) {
+                String ans = sc.nextLine();
+                if (q.isValidAnswer(ans)) {
+                    break;
+                } else {
+                    System.out.println("Invalid answer :(");
+                }
+
+            }
+
+        }
+
+        System.out.println("done!");
+    }
+
 }
---
+++ app/src/main/java/com/se/Question.java
@@ -0,0 +1,49 @@
+package com.se;
+
+public class Question {
+
+    public String text;
+    public Format format;
+
+    public Question(String text, Format format) {
+        this.text = text;
+        this.format = format;
+    }
+
+    public boolean isValidAnswer(String ans) {
+
+        if (ans == null) {
+            return false;
+        }
+
+        if (ans.isEmpty()) {
+            return false;
+        }
+
+        switch (format) {
+            case NUM -> {
+                try {
+                    Integer.parseInt(ans);
+                    return true;
+                } catch (NumberFormatException e) {
+                    return false;
+                }
+            }
+            case YN -> {
+                return ans.equalsIgnoreCase("Y") ||
+                        ans.equalsIgnoreCase("N") ||
+                        ans.equalsIgnoreCase("yes") ||
+                        ans.equalsIgnoreCase("no");
+            }
+            default -> { // if format is txt
+                return true;
+            }
+        }
+
+    }
+
+    enum Format {
+        YN, NUM, TXT
+    }
+
+}
